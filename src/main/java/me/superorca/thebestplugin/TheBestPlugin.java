0670910586f9f25b4c19e44ef4c5dbe2160b0f6e
SuperOrca <69650371+SuperOrca@users.noreply.github.com>
2021-03-09 19:37:36 -0500

    Initial commit

---
+++ src/main/java/me/superorca/thebestplugin/TheBestPlugin.java
@@ -0,0 +1,12 @@
+package me.superorca.thebestplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TheBestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "stop");
+    }
+}
